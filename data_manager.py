c73021c68dd8c1ad9bd6bde2b73c8065ac11352b
Ernesto Zarza <ernestozarza91@yahoo.com>
2020-06-26 16:49:27 +0200

    Modifying the data manager script in order to get a custom dataframe

--- data_manager.py
+++ data_manager.py
@@ -66,4 +66,47 @@ def save_data():
 
         db_connection.close()
 
-save_data()
+
+def get_data_from_db(name=None, club=None, nationality=None):
+    sql_engine = create_engine('mysql+pymysql://root:Panda@#35@127.0.0.1', pool_recycle=3600)
+
+    db_connection = sql_engine.connect()
+
+    where = ''
+
+    if name:
+        where = "WHERE Name LIKE '{name}'".format(name=name)
+    if club:
+        if "WHERE" in where:
+            where += " AND Club LIKE '{club}'".format(club=club)
+        else:
+            where = "WHERE Club LIKE '{club}'".format(club=club)
+    if nationality:
+        if "WHERE" in where:
+            where += " AND Nationality LIKE '{nationality}'".format(nationality=nationality)
+        else:
+            where = "WHERE Nationality LIKE '{nationality}'".format(nationality=nationality)
+
+    frame = pd.read_sql("SELECT * FROM football_players_db.players {filter}".format(filter=where), db_connection);
+
+    pd.set_option('display.expand_frame_repr', False)
+
+    result = frame[['Name', 'Age', 'Nationality', 'Club', 'Photo', 'Overall', 'Value']]
+    result = result.to_json(force_ascii=False)
+
+    db_connection.close()
+    return result
+
+
+def get_data_frame_from_db():
+    sql_engine = create_engine('mysql+pymysql://root:Panda@#35@127.0.0.1', pool_recycle=3600)
+    db_connection = sql_engine.connect()
+    frame = pd.read_sql("SELECT * FROM football_players_db.players", db_connection);
+    pd.set_option('display.expand_frame_repr', False)
+    # result = frame[['Name', 'Age', 'Nationality', 'Club', 'Photo', 'Overall', 'Value']]
+    db_connection.close()
+    return frame
+
+
+if __name__ == '__main__':
+    save_data()
