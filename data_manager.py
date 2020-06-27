d41f2a07c1a9c10f756f124fc70ad4ac9cc31ab3
Ernesto Zarza <ernestozarza91@yahoo.com>
2020-06-27 12:43:25 +0200

    Creating a data manager class

--- data_manager.py
+++ data_manager.py
@@ -7,129 +7,147 @@ import pandas as pd
 basePath = os.path.dirname(os.path.abspath(__file__))
 
 
-def get_value_int(value):
-    """
-    Method to convert player's values into float
-    :param value: string value
-    :return: string value converted to float
-    """
-    magnitude = ''
-    if len(value) > 2:
-        if str(value[-1]).isalpha():
-            magnitude = value[-1]
-            v = value[1:-1]
+class DBConfig:
+    #'mysql+pymysql://root:Panda@#35@0.0.0.0:3309/football_players_db', pool_recycle=3600)
+    DB_USERNAME = 'root'
+    DB_PASSWORD = 'Panda@#35'
+    DATABASE_NAME = 'football_players_db'
+    DB_HOST = '0.0.0.0'
+    DB_URI = "mysql+pymysql://{user}:{password}@{host}/{db_name}".format(user=DB_USERNAME, password=DB_PASSWORD,
+                                                                         host=DB_HOST, db_name=DATABASE_NAME)
+    MYSQL_ROOT_PASSWORD = 'Panda@#35'
+    MYSQL_USER = 'root'
+    MYSQL_ALLOW_EMPTY_PASSWORD = True
+
+    SQLALCHEMY_DATABASE_URI = DB_URI
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+class DataManager:
+
+    def __init__(self):
+        self.db_config = DBConfig()
+
+    def get_value_int(self, value):
+        """
+        Method to convert player's values into float
+        :param value: string value
+        :return: string value converted to float
+        """
+        magnitude = ''
+        if len(value) > 2:
+            if str(value[-1]).isalpha():
+                magnitude = value[-1]
+                v = value[1:-1]
+            else:
+                v = value[1:]
         else:
-            v = value[1:]
-    else:
-        v = value[1]
+            v = value[1]
 
-    data = float(v)
+        data = float(v)
 
-    if magnitude == 'M':
-        data *= 10**6
-    elif magnitude == 'K':
-        data *= 10**3
-    return data
+        if magnitude == 'M':
+            data *= 10**6
+        elif magnitude == 'K':
+            data *= 10**3
+        return data
 
+    def read_data_csv(self):
+        """
+        Method to read the original csv file
+        :return: a customized dataframe with the significant fields
+        """
+        path = basePath + '/data/data.csv'
+        df = pd.read_csv(path)
+        data_set = df[['ID', 'Name', 'Age', 'Nationality', 'Club', 'Photo', 'Overall', 'Value', 'Position']]
+        data_set['Value'] = data_set['Value'].apply(self.get_value_int)
+        return data_set
 
-def read_data_csv():
-    """
-    Method to read the original csv file
-    :return: a customized dataframe with the significant fields
-    """
-    path = basePath + '/data/data.csv'
-    df = pd.read_csv(path)
-    data_set = df[['ID', 'Name', 'Age', 'Nationality', 'Club', 'Photo', 'Overall', 'Value', 'Position']]
-    data_set['Value'] = data_set['Value'].apply(get_value_int)
-    return data_set
+    def save_data(self):
+        """
+        Save the data from the csv in the db
+        :return:
+        """
 
+        table_name = "Players"
 
-def save_data():
-    """
-    Save the data from the csv in the db
-    :return:
-    """
+        data_frame = self.read_data_csv()
 
-    table_name = "Players"
+        sql_engine = create_engine(self.db_config.SQLALCHEMY_DATABASE_URI, pool_recycle=3600)
 
-    data_frame = read_data_csv()
+        db_connection = sql_engine.connect()
 
-    sql_engine = create_engine('mysql+pymysql://root:Panda@#35@127.0.0.1/football_players_db', pool_recycle=3600)
+        try:
 
-    db_connection = sql_engine.connect()
+            frame = data_frame.to_sql(table_name, db_connection, if_exists='fail')
 
-    try:
+        except ValueError as error:
 
-        frame = data_frame.to_sql(table_name, db_connection, if_exists='fail')
+            print(error)
 
-    except ValueError as error:
+        except Exception as ex:
 
-        print(error)
+            print(ex)
 
-    except Exception as ex:
+        else:
 
-        print(ex)
+            print("Table %s created successfully." % table_name);
 
-    else:
+        finally:
 
-        print("Table %s created successfully." % table_name);
+            db_connection.close()
 
-    finally:
+    def get_data_from_db(self, name=None, club=None, nationality=None):
+        """
+        Filter method
+        :param name: filter by name
+        :param club: filter by club
+        :param nationality: filter by nationality
+        :return: customized dataframe
+        """
+        sql_engine = create_engine(self.db_config.SQLALCHEMY_DATABASE_URI, pool_recycle=3600)
+
+        db_connection = sql_engine.connect()
+
+        where = ''
+
+        # Next steps make the search by patterns
+        if name:
+            where = "WHERE Name LIKE '{name}'".format(name=name)
+        if club:
+            if "WHERE" in where:
+                where += " AND Club LIKE '{club}'".format(club=club)
+            else:
+                where = "WHERE Club LIKE '{club}'".format(club=club)
+        if nationality:
+            if "WHERE" in where:
+                where += " AND Nationality LIKE '{nationality}'".format(nationality=nationality)
+            else:
+                where = "WHERE Nationality LIKE '{nationality}'".format(nationality=nationality)
+        query = "SELECT * FROM football_players_db.players {filter}".format(filter=where)
+
+        frame = pd.read_sql(query, db_connection)
+
+        pd.set_option('display.expand_frame_repr', False)
+
+        result = frame[['Name', 'Age', 'Nationality', 'Club', 'Photo', 'Overall', 'Value']]
+        result = result.to_json(force_ascii=False)
 
         db_connection.close()
+        return result
 
-
-def get_data_from_db(name=None, club=None, nationality=None):
-    """
-    Filter methos
-    :param name: filter by name
-    :param club: filter by club
-    :param nationality: filter by nationality
-    :return: customized dataframe
-    """
-    sql_engine = create_engine('mysql+pymysql://root:Panda@#35@127.0.0.1', pool_recycle=3600)
-
-    db_connection = sql_engine.connect()
-
-    where = ''
-
-    # Next steps make the search by patterns
-    if name:
-        where = "WHERE Name LIKE '{name}'".format(name=name)
-    if club:
-        if "WHERE" in where:
-            where += " AND Club LIKE '{club}'".format(club=club)
-        else:
-            where = "WHERE Club LIKE '{club}'".format(club=club)
-    if nationality:
-        if "WHERE" in where:
-            where += " AND Nationality LIKE '{nationality}'".format(nationality=nationality)
-        else:
-            where = "WHERE Nationality LIKE '{nationality}'".format(nationality=nationality)
-    query = "SELECT * FROM football_players_db.players {filter}".format(filter=where)
-
-    frame = pd.read_sql(query, db_connection)
-
-    pd.set_option('display.expand_frame_repr', False)
-
-    result = frame[['Name', 'Age', 'Nationality', 'Club', 'Photo', 'Overall', 'Value']]
-    result = result.to_json(force_ascii=False)
-
-    db_connection.close()
-    return result
-
-
-def get_data_frame_from_db():
-    """
-    :return: returns the db as a dataframe
-    """
-    sql_engine = create_engine('mysql+pymysql://root:Panda@#35@127.0.0.1', pool_recycle=3600)
-    db_connection = sql_engine.connect()
-    frame = pd.read_sql("SELECT * FROM football_players_db.players", db_connection);
-    pd.set_option('display.expand_frame_repr', False)
-    db_connection.close()
-    return frame
+    def get_data_frame_from_db(self):
+        """
+        :return: returns the db as a dataframe
+        """
+        sql_engine = create_engine(self.db_config.SQLALCHEMY_DATABASE_URI, pool_recycle=3600)
+        db_connection = sql_engine.connect()
+        frame = pd.read_sql("SELECT * FROM football_players_db.players", db_connection);
+        pd.set_option('display.expand_frame_repr', False)
+        db_connection.close()
+        return frame
 
 
 if __name__ == '__main__':
-    save_data()
+    data_manager = DataManager()
+    data_manager.save_data()
